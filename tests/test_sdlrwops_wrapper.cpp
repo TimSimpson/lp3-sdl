7cdd451c890989d06accb625579aaf0751beb1c2
Tim Simpson <timsimpson4@gmail.com>
2020-03-01 00:39:08 -0600

    adds options for skipping unit tests

---
+++ tests/test_sdlrwops_wrapper.cpp
@@ -0,0 +1,20 @@
+#include <lp3/sdl.hpp>
+#include <string>
+#include <vector>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+TEST_CASE("Test RWOPs wrappers", "[read_a_file]") {
+
+    // auto story = media.load("core/story.txt");
+
+    // char content[16];
+    // std::size_t read_size = story.read(content, 1, 16);
+
+    // REQUIRE(std::size_t{16} == read_size);
+
+    // std::string contents(content, read_size);
+    // REQUIRE(contents == "Romulus and Remu");
+    REQUIRE(2 == 2);
+}
+// ~end-doc
