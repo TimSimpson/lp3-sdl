2b2d1cb14f05b281bcddae8319975f9775e52469
Tim Simpson <timsimpson4@gmail.com>
2020-03-01 01:08:22 -0600

    write a half-ok test for RWops stuff

--- tests/test_sdlrwops_wrapper.cpp
+++ tests/test_sdlrwops_wrapper.cpp
@@ -5,16 +5,47 @@
 #include <catch2/catch.hpp>
 
 TEST_CASE("Test RWOPs wrappers", "[read_a_file]") {
+    const std::string text("Hi, how's it going?");
 
-    // auto story = media.load("core/story.txt");
+    lp3::sdl::RWops rwops(SDL_RWFromConstMem(text.data(), text.size()));
 
-    // char content[16];
-    // std::size_t read_size = story.read(content, 1, 16);
+    {
+        char hi[2];
+        const std::size_t read_size = rwops.read(hi, 1, 2);
+        REQUIRE(2 == read_size);
+        REQUIRE('H' == hi[0]);
+        REQUIRE('i' == hi[1]);
+    }
 
-    // REQUIRE(std::size_t{16} == read_size);
+    {
+        const std::int64_t tell = rwops.seek(2);
+        REQUIRE(4 == tell);
+    }
 
-    // std::string contents(content, read_size);
-    // REQUIRE(contents == "Romulus and Remu");
-    REQUIRE(2 == 2);
+    {
+        struct Hows{
+            char text[5];
+        } hows;
+
+        const auto result = rwops.read(&hows, sizeof(hows));
+        REQUIRE(1 == result);
+        std::string expected("how's");
+        REQUIRE(std::string_view(hows.text, 5) == expected);
+    }
+
+    {
+        struct Rest{
+            char text[10];
+        };
+
+        const auto result = rwops.read_optional<Rest>();
+        REQUIRE((bool) result);
+        std::string expected(" it going?");
+        REQUIRE(std::string_view(result->text, 10) == expected);
+
+        // Won't work a second time
+        const auto result2 = rwops.read_optional<Rest>();
+        REQUIRE(! (bool) result2);
+    }
 }
 // ~end-doc
